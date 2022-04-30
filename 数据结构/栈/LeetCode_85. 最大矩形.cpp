5338ca6f7f0a98047168081c34eb0474d60b4f33
X_P <82564356+XP-3741@users.noreply.github.com>
2022-04-30 17:36:10 +0800

    Create LeetCode_85. 最大矩形.cpp

---
+++ 数据结构/栈/LeetCode_85. 最大矩形.cpp
@@ -0,0 +1,53 @@
+/*
+  题目描述：
+    给定一个仅包含 0 和 1 、大小为 rows x cols 的二维二进制矩阵，找出只包含 1 的最大矩形，并返回其面积。
+    
+  提示：
+    rows == matrix.length
+    cols == matrix[0].length
+    1 <= row, cols <= 200
+    matrix[i][j] 为 '0' 或 '1'
+*/
+
+/*
+  总结：
+    未懂
+*/
+
+/* 官方答案 */
+class Solution {
+public:
+    int maximalRectangle(vector<vector<char>>& matrix) {
+        int m = matrix.size();
+        if (m == 0) {
+            return 0;
+        }
+        int n = matrix[0].size();
+        vector<vector<int>> left(m, vector<int>(n, 0));
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == '1') {
+                    left[i][j] = (j == 0 ? 0: left[i][j - 1]) + 1;
+                }
+            }
+        }
+
+        int ret = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == '0') {
+                    continue;
+                }
+                int width = left[i][j];
+                int area = width;
+                for (int k = i - 1; k >= 0; k--) {
+                    width = min(width, left[k][j]);
+                    area = max(area, (i - k + 1) * width);
+                }
+                ret = max(ret, area);
+            }
+        }
+        return ret;
+    }
+};
